4c8fa2c22e206ffa318168bc10915a16f8754990
Mario Reder <mario.reder@pm.me>
2026-04-08 10:18:41 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "DEX Trading with Zypto",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Vaults,Leaderboard",
+  "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Vaults,Leaderboard,Campaigns",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "true",
   "VITE_ENABLE_CAMPAIGNS": "false",
